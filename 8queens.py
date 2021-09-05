c7017a4c90283eab6b533f4e8ea48aaf8813d1d3
meng <meng@103.229.212.71>
2021-09-06 00:17:52 +0800

    update

--- 8queens.py
+++ 8queens.py
@@ -1,30 +1,31 @@
-rows = 5
+size = 5
 
-a = [0 for i in range(rows)]
-b = [0 for i in range(rows)]
-c = [0 for i in range(rows * 2)]
-d = [0 for i in range(rows * 2)]
+a = [0 for i in range(size)]        # 每一行的皇后位于第几列
+b = [0 for i in range(size)]        # 哪一列被皇后占据了
+c = [0 for i in range(size * 2)]    # 哪个对角线被占据了 
+d = [0 for i in range(size * 2)]    #
 
 sum = 0
 
 result = []
 
-def Search(i):
-    global a, b, c, d, rows
-    for j in range(0, rows):
-        if b[j] == 0 and c[i + j] == 0 and d[i - j + rows] == 0:
+def Search(i):      # 当前考察第i行
+    global a, b, c, d, size
+    for j in range(0, size):
+        if b[j] == 0 and c[i + j] == 0 and d[i - j + size] == 0:
             a[i] = j
             b[j] = 1
             c[i + j] = 1
-            d[i - j + rows] = 1
-            if i == rows - 1:
+            d[i - j + size] = 1
+            if i == size - 1:
                 Output()
             else:
                 Search(i + 1)
 
+            # 回溯点
             b[j] = 0
             c[i + j] = 0
-            d[i - j + rows] = 0
+            d[i - j + size] = 0
 
 def Output():
     global sum
